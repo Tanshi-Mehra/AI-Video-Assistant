a90cabe6d83c215d7cfea725fb98183e14beacd6
Tanshi Mehra <tanshimehra547@gmail.com>
2026-08-04 16:41:48 +0530

    update utils

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -18,6 +18,10 @@ def download_youtube_audio(url :str) ->str:
             }
         ],
         "quiet": True,
+
+        'http_headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    }
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
